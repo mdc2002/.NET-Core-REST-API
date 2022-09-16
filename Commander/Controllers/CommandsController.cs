6ec2c9d555d25812da913e5969b9f6d1b3864f8c
mdc2002 <miko.christodoulou@gmail.com>
2022-09-16 15:13:27 +0300

    fixed issue

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -5,9 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace Commander.Controllers
 {
     //this class is going to inherity from a base class called Controller
+    [Route("api/commands")] //how do you get to the resources and the API endpoints within my controller
     //also we want to decorate our class theme with an API controller attribute (gives us some out of the box behaviours that our controller will perform)
     [ApiController]
-    [Route("api/commands")] //how do you get to the resources and the API endpoints within my controller
     public class CommandsController : ControllerBase
     // ControllerBase --> A base class for an MVC controller without view support
     {
